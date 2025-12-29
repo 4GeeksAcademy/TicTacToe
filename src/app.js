0ef86d073c4d997a602e4e782aaa26f0a8b32a43
MBello21 <magarbello@gmail.com>
2025-12-29 17:00:32 +0000

    TicTacToe

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
 import "bootstrap";
 import "./style.css";
 
+let startingBoard = ["", "", "", "", "", "", "", "", ""]
+let actualPlayer = "X"
+let activeGame = true
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const winningCombinations = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+]
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const motion = (e) => {
+  const element = e.currentTarget;
+  const index = element.getAttribute('data-index');
+
+  if (startingBoard[index] !== "" || !activeGame) return;
+
+  startingBoard[index] = actualPlayer;
+  element.innerHTML = `<h1>${actualPlayer}</h1>`;
+  checkResult();
+}
+
+const checkResult = () => {
+  let roundWin = false
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i]
+    if (startingBoard[a] && startingBoard[a] === startingBoard[b] && startingBoard[a] === startingBoard[c]) {
+      roundWin = true
+    }
+  }
+  if (roundWin) {
+    alert(`Player ${actualPlayer} has won.`)
+    activeGame = false
+    return
+  }
+  if (!startingBoard.includes("")) {
+    alert(`Draw!`)
+  }
+  actualPlayer = actualPlayer === "X" ? "O" : "X";
+}
+
+document.getElementById('restart').addEventListener('click', () => {
+  startingBoard = ["", "", "", "", "", "", "", "", ""];
+  activeGame = true;
+  actualPlayer = "X";
+  document.querySelectorAll('.box').forEach(b => b.innerHTML = "");
+})
+document.querySelectorAll('.box').forEach(box => {
+  box.addEventListener('click', motion);
+});
